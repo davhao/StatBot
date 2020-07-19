587d8f8cb02784662f269ef6640b8869dcf5a854
David Hao <djhao@live.unc.edu>
2020-07-19 17:12:35 -0400

    forgot to switch keys back to env vars, oops

--- config.js
+++ config.js
@@ -1,7 +1,5 @@
 module.exports = {
 	prefix       : 'stats',
-	// discordToken : process.env.discordToken,
-	// riotToken    : process.env.riotToken
-	discordToken : 'NzM0MTA3MjExODk4NTUyMzcy.XxR-wA.t9nMj3rgDoiM2TA0FFBofNKaALs',
-	riotToken    : 'RGAPI-564eef5b-608a-4284-ad16-6248bda1ae6a'
+	discordToken : process.env.discordToken,
+	riotToken    : process.env.riotToken
 };
